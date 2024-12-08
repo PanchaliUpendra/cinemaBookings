f58830cced11a41aaa2b78363ea2f2790bc239d7
harikeerthi24 <hanieleven@gmail.com>
2024-12-08 19:56:34 +0530

    login page completed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,37 @@
 import React from "react";
 import './Login.css';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 
-function Login(){
-    return(
+function Login() {
+    return (
         <>
-            <h1>login page</h1>
+            <div className="login-maincon">
+                <div className="green-shade top-right"></div>
+                <div className="green-shade bottom-left"></div>
+
+                <div className="login-container">
+                    <form className="login-form">
+                        <h2>Login to your account</h2>
+                        <div className="login-form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" placeholder="Enter your email" />
+                        </div>
+                        <div className="login-form-group">
+                            <label htmlFor="password">Password</label>
+                            <div className="login-password-container">
+                                <input type="password" id="password" placeholder="Enter your password" />
+                                <button type="button" className="login-password">
+                                    <RemoveRedEyeOutlinedIcon />
+                                </button>
+                            </div>
+                        </div>
+                        <button type="submit" className="login-btn">Login now</button>
+                        <p className="login-register-link">
+                            Don’t Have An Account? <span>Register Here</span>
+                        </p>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
