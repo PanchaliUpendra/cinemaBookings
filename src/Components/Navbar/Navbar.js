1b483a3ec84a4c8a5b6e9f8541087efc7cb19f7c
PanchaliUpendra <upendrapanchali83@gmail.com>
2024-12-08 12:18:30 +0530

    added the navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import './Navbar.css';
 import Cinemalogo from '../../Assets/cinemalogo.png';
+import { useNavigate } from "react-router-dom";
 
 
 function Navbar(){
+    const navigate = useNavigate();
     return(
         <>
             <div className="navbar-con">
@@ -11,8 +13,8 @@ function Navbar(){
                     <img src={Cinemalogo} alt='cinema logo' className="nav-cine-logo"/>
                 </div>
                 <div className="navbar-login">
-                    <p>login</p>
-                    <p>register</p>
+                    <p onClick={()=>navigate('/login')}>login</p>
+                    <p onClick={()=>navigate('/register')}>register</p>
                 </div>
             </div>
         </>
